bedaa986d84b17ef387b3452d1cfc3ca7d7bb696
Althaf vc <althafvellanchola46@gmail.com>
2024-12-01 01:41:17 +0530

    mongodb url changed back again

--- Api/index.js
+++ Api/index.js
@@ -1,7 +1,7 @@
 // Api.js
 import axios from 'axios';
 
-const BASEURL = "https://pointsapp-backend-new.onrender.com";
+const BASEURL = "https://pointsapp-backend-gau6.onrender.com";
 
 const api = axios.create({
   baseURL: BASEURL ? BASEURL : "http://192.168.96.221:3000", // Fallback URL
